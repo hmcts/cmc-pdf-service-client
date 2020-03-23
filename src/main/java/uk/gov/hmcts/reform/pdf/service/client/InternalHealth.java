9006cb2a095cb1b2bf689822abeae68ee63c014e
Sabah Irfan <sabah.irfan@gmail.com>
2020-03-23 10:12:58 +0000

    refactor/improve-health-indicator-reseliance

--- src/main/java/uk/gov/hmcts/reform/pdf/service/client/InternalHealth.java
+++ src/main/java/uk/gov/hmcts/reform/pdf/service/client/InternalHealth.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.pdf.service.client;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.actuate.health.Status;
 
@@ -10,24 +11,18 @@ import java.util.Map;
  * Can't manage to serialise the spring health class.
  * Jackson-java-8 modules constructor parameters won't work either :(
  */
-class InternalHealth {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InternalHealth {
+
     private final Status status;
-    private final Map<String, Object> details;
 
     @JsonCreator
-    public InternalHealth(
-        @JsonProperty("status") Status status,
-        @JsonProperty("details") Map<String, Object> details
-    ) {
+    public InternalHealth(@JsonProperty("status") Status status) {
         this.status = status;
-        this.details = details;
     }
 
     public Status getStatus() {
         return status;
     }
-
-    public Map<String, Object> getDetails() {
-        return details;
-    }
 }
+
