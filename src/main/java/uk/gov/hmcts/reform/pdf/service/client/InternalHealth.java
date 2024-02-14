17cccae65b4b6048ba4002a849c8f941fa9f4d89
Michael1142 <michael.johnson9@justice.gov.uk>
2024-02-14 16:35:12 +0000

    Removed unused import

--- src/main/java/uk/gov/hmcts/reform/pdf/service/client/InternalHealth.java
+++ src/main/java/uk/gov/hmcts/reform/pdf/service/client/InternalHealth.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.actuate.health.Status;
 
-import java.util.Map;
-
 /**
  * Can't manage to serialise the spring health class.
  * Jackson-java-8 modules constructor parameters won't work either :(
